630f3952d295a0a88c8ad15c0040958b3bd1be63
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2022-04-23 12:13:12 +0200

    listando libros usuario

--- locallibrary/locallibrary/catalog/models.py
+++ locallibrary/locallibrary/catalog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.urls import reverse #Used to generate URLs by reversing the URL patterns
 import uuid # Requerida para las instancias de libros únicos
-
+from django.contrib.auth.models import User  # Required to assign User as a borrower
+from datetime import date
 
 # Create your models here.
 class Genre(models.Model):
@@ -74,6 +75,13 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
